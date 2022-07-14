cb4b18bb7d9db4d4860f8a013d439ebbe09a773d
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2022-07-14 13:09:54 -0600

    Removed gray bubble behind button
    Buttonstyle -> PlainButtonStyle

---
+++ -ranDNumWatchApp- WatchKit Extension/randButtonDistApp.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+@main
+struct randButtonDistApp: App {
+    @SceneBuilder var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+
+        WKNotificationScene(controller: NotificationController.self, category: "myCategory")
+    }
+}
